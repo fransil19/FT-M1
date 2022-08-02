02631c17ecd8f6ae1d679a725f367c5490f252a0
Franco <xeneise19@gmail.com>
2022-08-02 20:52:45 -0300

    Se realiza homework de 01-IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  let lista = num.split("")
+  
+  let nueva_lista = lista.map((numero,indice) => {
+    return 2**((lista.length-1)-indice) * Number(numero)
+  })
 
+  let resultado = nueva_lista.reduce((suma,valor) => {
+    return suma + valor
+  },0)
+
+  return resultado
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  let lista = []
+  
+  while(num>0){
+    lista.unshift(num%2)
+    num = Math.floor(num/2)
+  }
 
+  let resultado = lista.join("")
+  return resultado
 }
 
 
